f6584927123042e45a0b1f03e99a9b8c2857a9c8
Vikash-Intern <vikash.singh@gistimpact.com>
2023-08-29 14:54:39 +0530

    added croos origin to remove cors error

--- src/main/java/com/gistimpact/carbonfootprint/controllers/FootprintController.java
+++ src/main/java/com/gistimpact/carbonfootprint/controllers/FootprintController.java
@@ -6,16 +6,12 @@ import com.gistimpact.carbonfootprint.service.CompanyListService;
 import com.gistimpact.carbonfootprint.service.FootPrintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class FootprintController {
-
 
     @Autowired
     private FootPrintService footPrintService;
